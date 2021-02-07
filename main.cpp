f02931e0c3ec474da1887741ec1b61d7781cb4cd
Naqmon <daniil.shtobbe@yandex.ru>
2021-02-07 22:46:16 +0300

    added function print

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
+void print(double x){
+	std::cout << x << "\n";
+}
+
 double sqr(double x){
 	return x*x;
 }
 
 int main(){
-	std::cout << "Hello, World\n";
+	print(sqr(3));
 	return 0;
 }
